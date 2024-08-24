0f4635cfebd440e4c9c5230e852e201b234be7ad
taiga-automate-growth <oshima@automate-growth.com>
2024-08-24 09:51:44 +0900

    update:regiterdJob変数をtry文の前に定義 try文終了後に使うため

--- src/Domain/Service/JobDuplicateChecker.js
+++ src/Domain/Service/JobDuplicateChecker.js
@@ -7,23 +7,23 @@ class JobDuplicateChecker{
    * @param {Map<number,Job>} jobs - 案件配列
    */
   handle(jobs){
-
+    let registerdJobs;
     try{
       registerdJobs = this.repository.findAll();
     }catch(e){
+      console.log(e);
       return jobs;
     }
-      
+
     for(const [key,job] of jobs){
-
       for(const registerdJob of registerdJobs.values()){
-        if(registerdJob.getNumber() !== job.getNumber()) continue;
-        if(registerdJob.getSite() !== job.getSite()) continue;
-
+        if(registerdJob.getNumber() != job.getNumber()) continue;
+        if(registerdJob.getSite() != job.getSite()) continue;
+        
         jobs.delete(key);
         break;
       }
-    }  
+    } 
     return jobs;
   }
 }
