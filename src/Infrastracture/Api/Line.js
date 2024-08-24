7843dacc95c5516e2f2988a8c7e855ff3daae88c
taiga-automate-growth <oshima@automate-growth.com>
2024-08-24 21:18:06 +0900

    update:新規案件を通知するメソッドを変更 引数で渡される案件情報が配列ではなくMapオブジェクトに変わったため

--- src/Infrastracture/Api/Line.js
+++ src/Infrastracture/Api/Line.js
@@ -4,10 +4,13 @@ class Line{
   }
   
   /**
-   * @param {Array<Job>} jobs - 案件配列
+   * @param {Map<number,Job>} jobs - 案件配列
    */
   notifyNewJob(jobs){
-    const carousels = jobs.reduce((carousels, job) => {
+    let jobCount = `${jobs.size}件`;
+    const carousels = new Map();
+    
+    for(const job of jobs.values()){
       const title = job.getTitle();
       const site = job.getSite();
       const column = LineMessage.createColumn(site);
@@ -22,36 +25,52 @@ class Line{
       const generateSuggestionAction = LineMessage.createPostbackAction(`action=generateSuggestion&id=${id}`);
       column.addAction(generateSuggestionAction);
       generateSuggestionAction.setLabel('提案文を生成する');
-
-      const carouselCount = carousels.size;
-
-      if(carouselCount === 0){
-        const carousel = LineMessage.createCarousel([column]);
-        carousels.set(carouselCount + 1, carousel);
-        return carousels;
-      }
       
-      const lastCarousel = carousels.get(carouselCount);
-      console.log(lastCarousel.canAddColumn());
-      if(lastCarousel.canAddColumn()){
-        lastCarousel.addColumn(column);
-
-      }else{
+      if(!carousels.has('last')){
         const carousel = LineMessage.createCarousel([column]);
-        carousels.set(carouselCount + 1, carousel);
+        carousels.set('last',carousel);
+        continue;
+      }else{
+        const carousel = carousels.get('last');
+        if(carousel.canAddColumn()){
+          carousel.addColumn(column);
+          continue;
+        }else{
+          const key = carousels.size;
+          carousel.delete('last');
+          carousels.set(key,carousel);
+          const newCarousel = LineMessage.createCarousel([column]);
+          carousels.set('last',newCarousel);
+        }
       }
-
-      return carousels;
-    },new Map());
-
-    const templates = []
-    for(const carousel of carousels.values()){
-      const columnCount = carousel.countColumns();
-      const template = LineMessage.createTemplate(`新規案件が${columnCount}件あります`);
-      template.setTemplate(carousel);
-      templates.push(template);
     }
-    const userId = 'U75636e7098f9ec20dc1d6f3c353625c7';
-    const send = this.apiClient.sendPush(userId, templates);
+    
+    const messagesList = [];
+    for(const [key,carousel] of carousels){
+      if(key !== 'last'){
+        jobCount = '';
+      }
+      const template = LineMessage.createTemplate(`新規案件が${jobCount}あります`);
+      template.setTemplate(carousel);
+    
+      const messagesListCount = messagesList.length;
+      if(messagesListCount === 0){
+        messagesList.push([template]);
+        continue;
+      }
+    
+      const lastMessages = messagesList[messagesListCount - 1];
+      const lastMessagesCount = lastMessages.length;
+      if(lastMessagesCount <= 5){
+        lastMessages.push(template);
+      }else{
+        messagesList.push([template]);
+      }
+    }
   }
+  
+  messagesList.forEach(messages => {
+    const userId = 'U75636e7098f9ec20dc1d6f3c353625c7';
+    const send = this.apiClient.sendPush(userId, messages);
+  });
 }
