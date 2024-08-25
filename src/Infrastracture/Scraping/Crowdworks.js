cfb9ed7c32cf4e393b39c03cf79a685af60a97a2
taiga-automate-growth <oshima@automate-growth.com>
2024-08-25 19:09:26 +0900

    update:HTMLコンテンツの抽出をスーパークラスから継承したメソッドからライブラリに変更
    スクレイピングの実装をシステムから切り離すため

--- src/Infrastracture/Scraping/Crowdworks.js
+++ src/Infrastracture/Scraping/Crowdworks.js
@@ -46,7 +46,7 @@ class Crowdworks extends JobSite {
   getJobTitle(jobDetailContent){
     const startString = '<h1>';
     const endString = '<span class="subtitle">';
-    const jobTitle = this.parseText(jobDetailContent, startString, endString);
+    const jobTitle = HTMLPerser.byText(jobDetailContent, startString, endString);
     if(jobTitle.length > 0) {
       return jobTitle[0];
     }
@@ -61,7 +61,7 @@ class Crowdworks extends JobSite {
   getJobDetail(jobDetailContent){
     const startString = '<td class="confirm_outside_link">';
     const endString = '</td>';
-    const jobDetail = this.parseText(jobDetailContent, startString, endString);
+    const jobDetail = HTMLPerser.byText(jobDetailContent, startString, endString);
     if(jobDetail.length > 0){
       return jobDetail[0];
     }
@@ -75,7 +75,7 @@ class Crowdworks extends JobSite {
    */
   getDeadline(jobDetailContent){
     const regex = /<div>応募期限<\/div>\s*<\/th>\s*<td>(\d{4}年\d{2}月\d{2}日)<\/td>/g;
-    const deadline = this.parseRegex(jobDetailContent, regex);
+    const deadline = HTMLPerser.byRegex(jobDetailContent, regex);
     if(deadline.length > 0){
       return deadline[0];
     }
--- src/Infrastracture/Scraping/JobSite.js
+++ src/Infrastracture/Scraping/JobSite.js
@@ -14,60 +14,6 @@ class JobSite {
   }
 
   /**
-   * @param {string} content - 取得対象の文字列
-   * @param {string} startString - 取得開始文字列
-   * @param {string} endString - 取得終了文字列
-   * @return {Array<string>} 取得結果文字列の配列
-   */
-  parseText(content, startString,endString){
-    const results = [];
-    let startIndex = 0;
-
-    while (true) {
-      startIndex = content.indexOf(startString, startIndex);
-      if (startIndex === -1) break;
-
-      const endIndex = content.indexOf(endString, startIndex + startString.length);
-      if (endIndex === -1) break;
-
-      const extractedString = content.substring(startIndex + startString.length, endIndex);
-      results.push(this.trim(extractedString));
-
-      startIndex = endIndex + endString.length;
-    }
-
-    return results;
-  
-  }
-
-  parseRegex(content,regex){
-    const results = [];
-    let match;
-    
-    while ((match = regex.exec(content)) !== null) {
-      results.push(match[1].trim(match[1]));
-    }
-    
-    return results;
-
-  }
-
-  /**
-   * テキストを成形する
-   * 
-   * @param {string} text - 成形対象のテキスト
-   */
-  trim(text){
-    text = text.replace(/\n/g, '');
-  
-    text = text.replace(/<br\s*\/?>/g, '\n');
-    
-    text = text.replace(/<\/?[^>]+(>|$)/g, '');
-
-    return text;
-  }
-
-  /**
    * 案件の全ての情報を取得する
    *
    * @param {Job} job - 案件
