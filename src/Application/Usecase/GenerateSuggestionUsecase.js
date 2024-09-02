9bc5f4d8d64bbe1bdcb179dc3a6542f9a86d5d46
taiga-automate-growth <oshima@automate-growth.com>
2024-09-03 04:34:02 +0900

    update:提案文の返信方法をインフラ層に委譲

--- src/Application/Usecase/GenerateSuggestionUsecase.js
+++ src/Application/Usecase/GenerateSuggestionUsecase.js
@@ -5,8 +5,8 @@ class GenerateSuggestionUsecase{
         const apikey = PropertiesService.getScriptProperties().getProperty('gemini-api-key');
         this.gemini = Gemini.create(apikey);
         this.logger = DocumentLogger.create('1-8H6oA7bl8hVlGbhhlJvRyBqHFRPjgSk_RuW91P3UHg');
-        const accessToken = PropertiesService.getScriptProperties().getProperty('gemini-api-key');
-        this.line = LineBot.create(accessToken);
+        const accessToken = PropertiesService.getScriptProperties().getProperty('line-access-token');
+        this.line = new Line(accessToken);
     }
 
     handle(postbackData,replyToken){
@@ -14,8 +14,8 @@ class GenerateSuggestionUsecase{
         const job = this.jobRepository.findById(jobId);
         const achievements = this.achievementsRepository.find();
         const prompt = new Prompt().forGenerateSuggestion(job,achievements);
-        const answer = this.gemini.sendTextPrompt(prompt);
-        logger.log(answer);
-        const reply = this.line.reply(replyToken)
+        const suggestion = this.gemini.sendTextPrompt(prompt);
+        logger.log(suggestion);
+        const reply = this.line.sendSuggestion(suggestion,replyToken);
     }
 }
