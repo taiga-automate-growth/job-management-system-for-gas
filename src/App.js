39dd8bea2fc4a239a8d9c330f7bc3b16013547e6
taiga-automate-growth <oshima@automate-growth.com>
2024-09-04 12:41:28 +0900

    update:ポストリクエストにログ出力を追加 エラー時に原因を特定するため

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 function doPost(e){
-    const accessToken = PropertyService.getScriptProperties().getProperty('line-access-token');
-    const line = this.line.create(accessToken);
-    const webHookEvent = line.receiveEvent(e);
-    const senderId = webHookEvent.getSenderId();
-    line.loading(senderId, 60);
+    const logger = DocumentLogger.create('1-8H6oA7bl8hVlGbhhlJvRyBqHFRPjgSk_RuW91P3UHg');
+    logger.log('リクエストを受け付けたよ！！');
+    try {
+        
+        const accessToken = PropertiesService.getScriptProperties().getProperty('line-access-token');
+        const line = LineBot.create(accessToken);
+        const webHookEvent = line.receiveEvent(e);
+        const senderId = webHookEvent.getSenderId();
+        line.loading(senderId, 60);
 
-    if(webHookEvent.isPostbackEvent()){
-        const postbackData = webHookEvent.getPostbackData();
-        const replyToken = webHookEvent.getReplyToken();
+        if(webHookEvent.isPostbackEvent()){
+            const postbackData = webHookEvent.getPostbackData();
+            const replyToken = webHookEvent.getReplyToken();
 
-        if(postbackData.action === 'generateSuggestion'){
-            new GenerateSuggestionUsecase().handle(postbackData,replyToken);
-        }else if(postbackData.action === 'cancelSuggestion'){
-            new CancelSuggestionUsecase().handle(postbackData,replyToken);
+            if(postbackData.action === 'generateSuggestion'){
+                new GenerateSuggestionUsecase().handle(postbackData,replyToken);
+            }else if(postbackData.action === 'cancelSuggestion'){
+                new CancelSuggestionUsecase().handle(postbackData,replyToken);
+            }
         }
+    
+    } catch (error) {
+        logger.log(error);
     }
 }
 
